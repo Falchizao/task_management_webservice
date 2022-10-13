276a66cb80db0d161f53e429b74191dcdf9e5010
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-10-13 20:42:56 -0300

    feat: start task handler

--- src/api/task.rs
+++ src/api/task.rs
@@ -114,3 +114,16 @@ pub async fn submit_task(
         Err(_) => Err(TaskError::TaskCreationFailure)
     }
 }
+
+#[put("/task/{task_global_id}/start")]
+pub async fn start_task(
+    ddb_repo: Data<DDBRepository>, 
+    task_identifier: Path<TaskIdentifier>
+) -> Result<Json<TaskIdentifier>, TaskError> {
+    state_transition(
+        ddb_repo, 
+        task_identifier.into_inner().task_global_id, 
+        TaskState::InProgress, 
+        None
+    ).await
+}
