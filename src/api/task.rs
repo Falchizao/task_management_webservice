959bd070cb3d13cccc40eb43e21fc3804f02ed1f
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-10-13 19:45:05 -0300

    feat: get route for global activity

--- src/api/task.rs
+++ src/api/task.rs
@@ -11,3 +11,36 @@ use actix_web::{
 };
 use serde::{Serialize, Deserialize};
 use derive_more::{Display};
+
+#[derive(Deserialize, Serialize)]
+pub struct TaskIdentifier {
+    activity_global_id: String,
+}
+
+#[derive(Deserialize)]
+pub struct TaskCompletionRequest {
+    result_file: String
+}
+
+#[derive(Deserialize)]
+pub struct SubmitTaskRequest {
+    user_id: String,
+    task_type: String,
+    source_file: String
+}
+
+
+#[get("/activity/{activity_global_id}")]
+pub async fn get_task(
+    ddb_repo: Data<DDBRepository>, 
+    task_identifier: Path<TaskIdentifier>
+) -> Result<Json<Task>, TaskError> {
+    let tsk = ddb_repo.get_task(
+        task_identifier.into_inner().activity_global_id
+    ).await;
+
+    match tsk {
+        Some(tsk) => Ok(Json(tsk)),
+        None => Err(TaskError::TaskNotFound)
+    }
+}
