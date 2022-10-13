5e5b7f513ab15892460d76da4d762448a79b47f2
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-10-13 19:53:08 -0300

    chore: ddb client injection

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,25 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
-
+    let config = aws_config::load_from_env().await;
     HttpServer::new(move || {
+        let ddb_repo: DDBRepository = DDBRepository::init(
+            String::from("task"),
+            config.clone()
+        );
+        let ddb_data = Data::new(
+            ddb_repo
+        );
         let logger = Logger::default();
         App::new()
             .wrap(logger)
+            .app_data(ddb_data)
             .service(get_task)
+            .service(submit_task)
+            .service(start_task)
+            .service(complete_task)
+            .service(pause_task)
+            .service(fail_task)
     })
     .bind(("127.0.0.1", 80))?
     .run()
