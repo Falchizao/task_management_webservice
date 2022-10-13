58e9d110645b3db0c469f6fc007ae6b16fd4bd80
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-10-13 19:28:48 -0300

    chore: Iteration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+
+use api::task::{
+    get_task
+};
+
+use actix_web::{HttpServer, App, web::Data, middleware::Logger};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    HttpServer::new(move || {
+        let logger = Logger::default();
+        App::new()
+            .wrap(logger)
+            .service(get_task)
+    })
+    .bind(("127.0.0.1", 80))?
+    .run()
+    .await
 }
